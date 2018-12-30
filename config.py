2345989ae9eacef27975244d65af410f8c80f49e
MobtgZhang <33253392+MobTgZhang@users.noreply.github.com>
2018-12-30 11:25:42 -0500

    Update config.py

--- config.py
+++ config.py
@@ -8,17 +8,20 @@ home = "/home/asus"
 # The model defination of the file
 segment_model_file = os.path.join(home,"ltp_data_v3.4.0")
 # The defination of the raw data ,the first data,named AI_Challenger2018
-train_file = os.path.join(home,"AI_Challenger2018","ai_challenger_sentiment_analysis_trainingset_20180816","sentiment_analysis_trainingset.csv")
-validation_file = os.path.join(home,"AI_Challenger2018","ai_challenger_sentiment_analysis_validationset_20180816","sentiment_analysis_validationset.csv")
-test_file = os.path.join(home,"AI_Challenger2018","ai_challenger_sentiment_analysis_testa_20180816","sentiment_analysis_testa.csv")
+train_file = os.path.join(home,"AI_Challenger2018","NewCode4","AI-Challenger2018","sentiment_analysis_trainingset.csv")
+validation_file = os.path.join(home,"AI_Challenger2018","NewCode4","AI-Challenger2018","sentiment_analysis_validationset.csv")
+test_file = os.path.join(home,"AI_Challenger2018","NewCode4","AI-Challenger2018","sentiment_analysis_testset.csv")
 # Save data files
-save_datafile = os.path.join(home,"AI_Challenger2018","data_processed")
+save_datafile = os.path.join(home,"AI_Challenger2018","NewCode4","data_processed")
 train_npz = os.path.join(home,save_datafile,"train_npz.npz")
 test_npz = os.path.join(home,save_datafile,"test_npz.npz")
 validation_npz = os.path.join(home,save_datafile,"validation_npz.npz")
-vocab_file = os.path.join(home,save_datafile,"vocab.txt")
+
+train_vocab_file = os.path.join(home,save_datafile,"train_vocab.txt")
+validate_vocab_file = os.path.join(home,save_datafile,"validate_vocab.txt")
+test_vocab_file = os.path.join(home,save_datafile,"test_vocab.txt")
 # The defination of cache file
-cache_file = os.path.join(home,"AI_Challenger2018","tmp")
+cache_file = os.path.join(home,"AI_Challenger2018","NewCode4","tmp")
 # The defination of the word embedding 
 wordembedding_file = os.path.join(home,"Word2Vec","sgns.baidubaike.bigram-char")
 flags.DEFINE_string("mode", "train", "train/data/debug/test")
@@ -33,7 +36,11 @@ flags.DEFINE_string("train_npz", train_npz, "")
 flags.DEFINE_string("test_npz", test_npz, "")
 flags.DEFINE_string("validation_npz", validation_npz, "")
 flags.DEFINE_string("save_datafile", save_datafile, "")
-flags.DEFINE_string("vocab_file", vocab_file, "")
+
+
+flags.DEFINE_string("train_vocab_file", train_vocab_file, "")
+flags.DEFINE_string("validate_vocab_file", validate_vocab_file, "")
+flags.DEFINE_string("test_vocab_file", test_vocab_file, "")
 
 flags.DEFINE_string("segment_model_file", segment_model_file, "")
 flags.DEFINE_string("wordembedding_file", wordembedding_file, "")
