18febc6c4b1d4da835b0d0a2877a187bad47ed5f
MobtgZhang <33253392+MobTgZhang@users.noreply.github.com>
2018-12-29 23:21:54 -0500

    Update preproc.py

--- preproc.py
+++ preproc.py
@@ -5,53 +5,11 @@ import pandas as pd
 import numpy as np
 import csv 
 import os
+import threading
 from config import config
-from utils import DataSetWordsLabel,DataSetEmddingLabel
-from zhon import hanzi
-import string
-def MakeVocabs(task_file,vocab_file):
-    print("Make vocabulary,Loading File:\n{}".format(task_file))
-    Vocabs = set()
-    with open(task_file,mode = "r",encoding = "utf-8") as fp:
-        DataSet = csv.reader(fp)
-        Headers =  next(DataSet)
-        for line in  tqdm(DataSet):
-            # id,sentence,labels
-            id_index = int(line[0])
-            sentence = line[1]
-            labels_index = line[2:-1]
-            # This is the English and Chinese punctuations
-            # print(string.punctuation)  //!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
-            # print(hanzi.punctuation)   //＂＃＄％＆＇（）＊＋，－／：；＜＝＞＠［＼］＾＿｀｛｜｝～｟｠｢｣､　、〃〈〉《》「」『』【】〔〕〖〗〘〙〚〛〜〝〞〟〰〾〿–—‘’‛“”„‟…‧﹏﹑﹔·！？｡。
-            # segment the sentences
-            segment = ltp.Segmentor()
-            segment.load(os.path.join(config.segment_model_file,"cws.model"))
-            sent = list(segment.segment(sentence))
-            segment.release()
-            for word in sent:
-                Vocabs.add(word.strip())
-    with open(vocab_file,mode = "w",encoding = "utf-8") as fp:
-        for _,word in enumerate(Vocabs):
-            fp.write(word + "\n")
-    Length = len(Vocabs)
-    del Vocabs
-    print("The vocabulary length:{}".format(Length))
-    print("Saved File:\n{}".format(vocab_file))
-# The first step we must make a vocabulary to modify the task
-def LoadVocabs(save_file = "vocab.txt"):
-    print("Loading vocabulary:\n{}".format(save_file))
-    Vocabs = set()
-    with open(save_file,mode = "r",encoding = "utf-8") as fpLoad:
-        while True:
-            line = fpLoad.readline()
-            if not line:
-                break
-            line = line.strip()
-            if line =="":
-                break
-            else:
-                Vocabs.add(line)
-    return Vocabs
+from utils import getRadomNum
+import thread_sepwords as thsepword
+import thread_sepsentences as thsepsent
 # Get word embedding 
 def GetEmbeddings(Vocabs,embedding_file):
     print("Loading Embedding Model .......")
@@ -68,7 +26,7 @@ def GetEmbeddings(Vocabs,embedding_file):
                 vec  = list(map(float, vectors[1:-1]))
             except:
                 pass
-            if word in Vocabs:
+            if word in Vocabs and len(vec) == config.word_dim:
                 word_vecs[word] = np.array(vec, dtype='float32')
     # add random vectors of unknown words which are not in pre-trained vector file.
     # if pre-trained vectors are not used, then initialize all words in vocab with random value.
@@ -76,26 +34,24 @@ def GetEmbeddings(Vocabs,embedding_file):
         if word not in word_vecs:
             word_vecs[word] = np.random.uniform(-0.25, 0.25, config.word_dim)
     return word_vecs
-def preproc(task_file,vocab_file):
+def preproc(task_file,save_file,seplength,delay):
     if not os.path.exists(config.save_datafile):
         os.mkdir(config.save_datafile)
     # Generate the vocabulary
-    if not os.path.exists(vocab_file):
-        MakeVocabs(task_file,vocab_file)
-    Vocabs = LoadVocabs()
-    # load embeddings
-    word_vecs = GetEmbeddings(Vocabs,config.wordembedding_file)
-    # MakeDataSet
-    datasetswords = DataSetWordsLabel(task_file)
-    # ChangeToEmbedding
-    datasetemdding = DataSetEmddingLabel(datasetswords,word_vecs)
-    if not os.path.exists(config.save_datafile):
-        os.mkdir(config.save_datafile)
-    np.savez(config.train_npz,datasetemdding = datasetemdding.Sentences)
+    dictionary = thsepword.Dictionary(task_file,config.vocab_file,"file",seplength,delay)
+    word_vecs = None
+    if not os.path.exists(save_file):
+        Vocabs = dictionary.getVocabs()
+        # load embeddings
+        word_vecs = GetEmbeddings(Vocabs,config.wordembedding_file)
+        # MakeDataSet
+        datasetswords = thsepsent.DataSetWords(task_file,save_file,word_vecs,"file",seplength,delay)
 def main(_):
     task_file = "/home/asus/AI_Challenger2018/TestData/testfile.csv"
-    preproc(task_file,config.vocab_file)
+    # save_file = "/home/asus/AI_Challenger2018/TestData/sent.npz"
+    seplength = 200
+    delay = 3
+    preproc(task_file,config.train_npz,seplength,delay)
     data = np.load(config.train_npz)
-    print(data['datasetemdding'])
 if __name__ == "__main__":
     app.run(main)
