11f7987669b923b3b61d31572f196ec23033b5b9
MobtgZhang <33253392+MobTgZhang@users.noreply.github.com>
2018-12-29 23:20:54 -0500

    Update config.py

--- config.py
+++ config.py
@@ -9,23 +9,40 @@ segment_model_file = os.path.join(home,"ltp_data_v3.4.0")
 train_file = os.path.join(home,"AI_Challenger2018","ai_challenger_sentiment_analysis_trainingset_20180816","sentiment_analysis_trainingset.csv")
 validation_file = os.path.join(home,"AI_Challenger2018","ai_challenger_sentiment_analysis_validationset_20180816","sentiment_analysis_validationset.csv")
 test_file = os.path.join(home,"AI_Challenger2018","ai_challenger_sentiment_analysis_testa_20180816","sentiment_analysis_testa.csv")
+# Save data files
+save_datafile = os.path.join(home,"AI_Challenger2018","data_processed")
+train_npz = os.path.join(home,save_datafile,"train_npz.npz")
+test_npz = os.path.join(home,save_datafile,"test_npz.npz")
+validation_npz = os.path.join(home,save_datafile,"validation_npz.npz")
+vocab_file = os.path.join(home,save_datafile,"vocab.txt")
+# The defination of cache file
+cache_file = os.path.join(home,"AI_Challenger2018","tmp")
 # The defination of the word embedding 
 wordembedding_file = os.path.join(home,"Word2Vec","sgns.baidubaike.bigram-char")
-flags.DEFINE_string("mode", "train", "train/debug/test")
-# The defination of cache file
-cache_file = os.path.join(home,"tmp")
-# The Processed Data
+flags.DEFINE_string("mode", "train", "train/data/debug/test")
 
-flags.DEFINE_string("segment_model_file", segment_model_file, "")
-flags.DEFINE_string("wordembedding_file", wordembedding_file, "")
-
+# The flag Data
 flags.DEFINE_string("train_file", train_file, "")
 flags.DEFINE_string("validation_file", validation_file, "")
 flags.DEFINE_string("test_file", test_file, "")
+flags.DEFINE_string("cache_file",cache_file,"")
+
+flags.DEFINE_string("train_npz", train_npz, "")
+flags.DEFINE_string("test_npz", test_npz, "")
+flags.DEFINE_string("validation_npz", validation_npz, "")
+flags.DEFINE_string("save_datafile", save_datafile, "")
+flags.DEFINE_string("vocab_file", vocab_file, "")
+
+flags.DEFINE_string("segment_model_file", segment_model_file, "")
+flags.DEFINE_string("wordembedding_file", wordembedding_file, "")
 
 flags.DEFINE_integer("word_dim",300, "Embedding dimension for word")
 flags.DEFINE_integer("char_dim",300, "Embedding dimension for char")
 flags.DEFINE_integer("text_length",1000, "the length of the text")
 
+flags.DEFINE_integer("train_word_sep_size",1000, "the batchsize to seperate raw data for word")
+flags.DEFINE_integer("train_delay",1000, "train raw dataset delay time")
+flags.DEFINE_integer("validation_delay",500, "validation raw dataset delay time")
+flags.DEFINE_integer("test_delay",500, "test raw dataset delay time")
 
 config = flags.FLAGS
