b724a831b2ede7f98267bc4aad8c433b96dcdd76
MobtgZhang <33253392+MobTgZhang@users.noreply.github.com>
2018-12-31 05:58:16 -0500

    Update model.py

--- model.py
+++ model.py
@@ -4,11 +4,13 @@ import torch.nn as nn
 import torch.nn.functional as F
 class SentimentNet(nn.Module):
     def __init__(self, vocab_size, embed_size, num_hiddens, num_layers,
-                 bidirectional, weight, labels, use_gpu, **kwargs):
+                 bidirectional, weight,word_to_idx,idx_to_word,labels, use_gpu):
         super(SentimentNet, self).__init__(**kwargs)
         self.num_hiddens = num_hiddens
         self.num_layers = num_layers
         self.use_gpu = use_gpu
+        self.word_to_idx = word_to_idx
+        self.idx_to_word = idx_to_word
         self.bidirectional = bidirectional
         self.embedding = nn.Embedding.from_pretrained(weight)
         self.embedding.weight.requires_grad = False
