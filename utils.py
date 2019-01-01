6b0a3ed0a834ef0515af95f92476c7180e3940ba
MobtgZhang <33253392+MobTgZhang@users.noreply.github.com>
2018-12-31 20:15:32 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,45 @@
-from config import config
 from tqdm import tqdm
 import uuid
 import pandas as pd
+import numpy as np
+from config import config
+# five score
+
+# Making token datasets
+def MakeSets(Vocabs):
+    word_to_idx = {word:k+1 for k,word in enumerate(Vocabs)}
+    word_to_idx['<unk>'] = 0
+    idx_to_word = {k+1:word for k,word in enumerate(Vocabs)}
+    idx_to_word[0] = '<unk>'
+    return idx_to_word,word_to_idx
+# Encoding the tokens
+def encode_samples(tokenized_samples,word_to_idx):
+    features = []
+    for sample in tokenized_samples:
+        feature = []
+        for token in sample:
+            if token in word_to_idx:
+                feature.append(word_to_idx[token])
+            else:
+                feature.append(0)
+        features.append(feature)
+    return features
+# Encoding the features
+def pad_samples(features, maxlen=1000, PAD=0):
+    padded_features = []
+    for feature in features:
+        if len(feature) >= maxlen:
+            padded_feature = feature[:maxlen]
+        else:
+            padded_feature = feature
+            while(len(padded_feature) < maxlen):
+                padded_feature.append(PAD)
+        padded_features.append(padded_feature)
+    return padded_features
+def prepare_vocab(Vocabs):
+    # Make a dictionary
+    idx_to_word,word_to_idx = MakeSets(Vocabs)
+    return idx_to_word,word_to_idx
 # random creates a file for every data file
 def getRadomNum():
     res = str(uuid.uuid4())
@@ -18,16 +56,11 @@ def GetData(filename,seplength):
         DataList.append(out.loc[k*seplength:(k+1)*seplength])
     return DataList
 # LoadVocabs
-def LoadVocabs(save_filename):
+def GetVocabs(filename):
     Vocabs = set()
-    with open(save_filename,mode = "r",encoding = "utf-8") as fpLoad:
-        while True:
-            line = fpLoad.readline()
-            if not line:
-                break
-            line = line.strip()
-            if line =="":
-                continue
-            else:
-                Vocabs.add(line)
+    data = np.load(filename)
+    sentences = list(data['sentences'])
+    for sent in sentences:
+        for word in sent:
+            Vocabs.add(word)
     return Vocabs
