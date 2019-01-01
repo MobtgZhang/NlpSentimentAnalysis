45a299a94132ee0508a3dead7c52908e8cee9713
MobtgZhang <33253392+MobTgZhang@users.noreply.github.com>
2018-12-31 20:11:42 -0500

    Update config.py

--- config.py
+++ config.py
@@ -3,25 +3,22 @@ import absl.flags as flags
 import torch
 import torch.backends.cudnn as cudnn
 
-# home = os.path.expanduser(".")
-home = "/home/asus"
+home = os.path.expanduser(".")
+# home = "/home/asus"
 # The model defination of the file
 segment_model_file = os.path.join(home,"ltp_data_v3.4.0")
 # The defination of the raw data ,the first data,named AI_Challenger2018
-train_file = os.path.join(home,"AI_Challenger2018","NewCode5","AI-Challenger2018","sentiment_analysis_trainingset.csv")
-validation_file = os.path.join(home,"AI_Challenger2018","NewCode5","AI-Challenger2018","sentiment_analysis_validationset.csv")
-test_file = os.path.join(home,"AI_Challenger2018","NewCode5","AI-Challenger2018","sentiment_analysis_testset.csv")
+train_file = os.path.join(home,"AI-Challenger2018","sentiment_analysis_trainingset.csv")
+validation_file = os.path.join(home,"AI-Challenger2018","sentiment_analysis_validationset.csv")
+test_file = os.path.join(home,"AI-Challenger2018","sentiment_analysis_testset.csv")
 # Save data files
-save_datafile = os.path.join(home,"AI_Challenger2018","NewCode5","data_processed")
+save_datafile = os.path.join(home,"data_processed")
 train_npz = os.path.join(home,save_datafile,"train_npz.npz")
 test_npz = os.path.join(home,save_datafile,"test_npz.npz")
 validate_npz = os.path.join(home,save_datafile,"validation_npz.npz")
 
-train_vocab_file = os.path.join(home,save_datafile,"train_vocab.txt")
-validate_vocab_file = os.path.join(home,save_datafile,"validate_vocab.txt")
-test_vocab_file = os.path.join(home,save_datafile,"test_vocab.txt")
 # The defination of cache file
-cache_file = os.path.join(home,"AI_Challenger2018","NewCode5","tmp")
+cache_file = os.path.join(home,"tmp")
 # The defination of the word embedding 
 wordembedding_file = os.path.join(home,"Word2Vec","sgns.baidubaike.bigram-char")
 flags.DEFINE_string("mode", "train", "train/data/debug/test")
@@ -36,11 +33,6 @@ flags.DEFINE_string("train_npz", train_npz, "")
 flags.DEFINE_string("test_npz", test_npz, "")
 flags.DEFINE_string("validate_npz", validate_npz, "")
 flags.DEFINE_string("save_datafile", save_datafile, "")
-
-
-flags.DEFINE_string("train_vocab_file", train_vocab_file, "")
-flags.DEFINE_string("validate_vocab_file", validate_vocab_file, "")
-flags.DEFINE_string("test_vocab_file", test_vocab_file, "")
 
 flags.DEFINE_string("segment_model_file", segment_model_file, "")
 flags.DEFINE_string("wordembedding_file", wordembedding_file, "")
@@ -61,15 +53,22 @@ flags.DEFINE_integer("batch_size",50, "the batchsize to seperate raw data for tr
 flags.DEFINE_float("learning_rate",0.8, "the learningrate for trainig data")
 
 # model save files
-save_statics_file = os.path.join(home,"AI_Challenger2018","NewCode5")
+save_statics_file = os.path.join(home,"log")
 model_save_file = os.path.join(home,save_statics_file,"model.pt")
-picture_save_file = os.path.join(home,save_statics_file,"pic.png")
+pic_trainloss_savefile = os.path.join(home,save_statics_file,"pic_train.png")
+pic_validateloss_savefile = os.path.join(home,save_statics_file,"pic_validate.png")
+pic_testloss_savefile = os.path.join(home,save_statics_file,"pic_test.png")
 
-flags.DEFINE_string("picture_save_file", picture_save_file, "")
+flags.DEFINE_string("save_statics_file",save_statics_file,"")
+flags.DEFINE_string("pic_trainloss_savefile", pic_trainloss_savefile, "")
+flags.DEFINE_string("pic_validateloss_savefile", pic_validateloss_savefile, "")
+flags.DEFINE_string("pic_testloss_savefile", pic_testloss_savefile, "")
 flags.DEFINE_string("model_save_file", model_save_file, "")
 
 config = flags.FLAGS
 device = "cuda" if torch.cuda.is_available() else "cpu"
+use_gpu = torch.cuda.is_available()
+flags.DEFINE_boolean("use_gpu",use_gpu,"")
 flags.DEFINE_string("device", device, "")
 
 cudnn.enabled = False
