9a1d8a76eb5922a7a6d5d6d1eb0740067ef54df5
MobtgZhang <33253392+MobTgZhang@users.noreply.github.com>
2019-01-09 03:41:32 -0500

    Update process.py

--- process.py
+++ process.py
@@ -58,19 +58,16 @@ def prepare_train(train_features,train_labels,validate_features,validate_labels,
     validate_iter = torch.utils.data.DataLoader(validate_set, batch_size=batch_size,shuffle=False)
     train_iter = torch.utils.data.DataLoader(train_set, batch_size=batch_size,shuffle=True)
     ## save the data
-    train_true_labels = train_labels.data.numpy()
-    train_length  = len(train_labels)
-    train_pred_labels = np.zeros((train_length,config.labels))
+    train_true_labels = []
+    train_pred_labels = []
 
-    validate_true_labels = validate_labels.data.numpy()
-    validate_length = len(validate_labels)
-    validate_pred_labels = np.zeros((validate_length,config.labels))
+    validate_true_labels = []
+    validate_pred_labels = []
 
+    dict_train = {}
+    dict_validate = {}
     train_loss_list = []
     validate_loss_list = []
-    list_train = []
-    list_validate = []
-
     for epoch in tqdm(range(num_epochs),"epoches: "):
         start = time.time()
         train_loss, validate_losses = 0, 0
@@ -87,16 +84,17 @@ def prepare_train(train_features,train_labels,validate_features,validate_labels,
             for name,p in net.named_parameters():
                 if p.requires_grad:ema.update_parameter(name,p)
             torch.nn.utils.clip_grad_norm_(net.parameters(),config.grad_clip)
+
+            # append the data 
             t_score = train_scaler.inverse_transform(score.data.numpy())
-            # append the data 
             train_pred_labels[(n-1)*batch_size:n*batch_size] = t_score.astype(int)
-
             t_score = np.squeeze(np.ceil(t_score.reshape(1,-1))).astype(int)
-            t_label = train_scaler.inverse_transform(label.data.numpy())
-            t_label = np.squeeze(t_label.reshape(1,-1)).astype(int)
-            # append the data 
-            train_true_labels[(n-1)*batch_size:n*batch_size] = t_label.astype(int)
 
+            # append the data 
+            t_label = train_scaler.inverse_transform(label.data.numpy())
+            train_true_labels[(n-1)*batch_size:n*batch_size] = t_label.astype(int)
+            t_label = np.squeeze(t_label.reshape(1,-1)).astype(int)
+            
             train_acc += accuracy_score(t_label,t_score)
             train_loss += loss
             train_loss_list.append(loss.data.numpy().tolist())
@@ -107,21 +105,26 @@ def prepare_train(train_features,train_labels,validate_features,validate_labels,
                 validate_loss = loss_function(validate_score, validate_label)
                 v_score = validate_score.data.numpy()
                 v_label = validate_label.data.numpy()
-                v_score = validate_scaler.inverse_transform(v_score)
-                # append the data 
-                validate_pred_labels[(m-1)*batch_size:m*batch_size] = v_score.astype(int)
 
+                # append the data
+                v_score = validate_scaler.inverse_transform(v_score)
+                validate_pred_labels[(m-1)*batch_size:m*batch_size] = v_score.astype(int)
                 v_score = np.squeeze(np.ceil(v_score.reshape(1,-1))).astype(int)
+
+                # append the data
                 v_label = validate_scaler.inverse_transform(v_label)
-                v_label = np.squeeze(v_label.reshape(1,-1)).astype(int)
-                # append the data 
                 validate_true_labels[(m-1)*batch_size:m*batch_size] = v_label.astype(int)
+                v_label = np.squeeze(v_label.reshape(1,-1)).astype(int)
 
                 validate_acc += accuracy_score(v_label,v_score)
                 validate_losses += validate_loss
                 validate_loss_list.append(validate_loss.data.numpy().tolist())
-        list_train.append(train_true_labels,train_pred_labels)
-        list_validate.append(validate_true_labels,validate_pred_labels)
+        dict_train['true'] = train_true_labels
+        dict_train['predict'] = train_pred_labels
+
+        dict_validate['true'] = validate_true_labels
+        dict_validate['predict'] = validate_pred_labels
+        
         end = time.time()
         runtime = end - start
         print('epoch: %d, train loss: %.4f, train acc: %.2f, validate loss: %.4f, validate acc: %.2f, time: %.2f' %
@@ -135,7 +138,7 @@ def prepare_train(train_features,train_labels,validate_features,validate_labels,
     net.to("cpu")
     torch.save(net,model_save_file)
     # save the loss value
-    np.savez(os.path.join(save_file,"labels.npz"),train_data = list_train,validate_data = list_validate)
+    np.savez(os.path.join(save_file,"labels.npz"),train_data = dict_train,validate_data = dict_validate)
     return train_loss_list,validate_loss_list
 def load_datasets(filename):
     data = np.load(filename)
@@ -230,7 +233,7 @@ def test_entry(modelname):
     m = 0
     test_loss_list = []
     loss_function = nn.BCELoss()
-    list_test = []
+    dict_test = {}
     with torch.no_grad():
         for test_feature, test_label in tqdm(test_iter,"test: "):
             m += 1
@@ -239,20 +242,21 @@ def test_entry(modelname):
 
             te_label = test_label.data.numpy()
             te_score = test_score.data.numpy()
-            # append the data 
-            test_pred_labels[(m-1)*config.batch_size:m*config.batch_size] = te_score.astype(int)
-
+            # append the data
             te_score = test_scaler.inverse_transform(te_score)
+            test_pred_labels[(m-1)*config.batch_size:m*config.batch_size] = te_score.astype(int)
             te_score = np.squeeze(np.round(te_score.reshape(1,-1))).astype(int)
+
+            # append the data
             te_label = test_scaler.inverse_transform(te_label)
-            te_label = np.squeeze(te_label.reshape(1,-1)).astype(int)
-            # append the data 
             test_true_labels[(m-1)*config.batch_size:m*config.batch_size] = te_label.astype(int)
+            te_label = np.squeeze(te_label.reshape(1,-1)).astype(int)
 
             test_acc += accuracy_score(te_label,te_score)
             test_losses += test_loss
             test_loss_list.append(test_loss.data.numpy().tolist())
-    list_test.append(test_true_labels,test_pred_labels)
+    dict_test['true'] = test_true_labels
+    dict_test['predict'] = test_pred_labels
     end = time.time()
     runtime = end - start
     print('test loss: %.4f, test acc: %.2f, time: %.2f' %(test_losses.data / m, test_acc / m, runtime))
@@ -261,7 +265,7 @@ def test_entry(modelname):
     data = np.load(os.path.join(save_file,"labels.npz"))
     list_train = data['train_data']
     list_validate = data['validate_data']
-    np.savez(os.path.join(save_file,"labels.npz"),train_data = list_train,validate_data = list_validate,test_data = list_test)
+    np.savez(os.path.join(save_file,"labels.npz"),train_data = list_train,validate_data = list_validate,test_data = dict_test)
     # draw pictures
     pic_save_file = os.path.join(config.save_statics_file,modelname)
     pic_test = os.path.join(pic_save_file,modelname+"_test"+".png")
