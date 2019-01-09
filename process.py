f0210ff83b1dc6a981fd6ebe928d51485f976b1a
MobtgZhang <33253392+MobTgZhang@users.noreply.github.com>
2019-01-09 10:55:23 -0500

    Update process.py

--- process.py
+++ process.py
@@ -15,7 +15,7 @@ import math
 from tqdm import tqdm
 from utils import GetVocabs,MakeSets,encode_samples,pad_samples,prepare_vocab,prepare_labels
 from config import config
-from model import BiLSTMNet,textCNN,BiGRUNet,EMA,MANNet,BiSRU
+from model import BiLSTMNet,textCNN,BiGRUNet,EMA,MANNet
 
 def prepare_datasets(sentences,labels,word_to_idx,mode):
     print("Preparing "+mode+" datasets ... ...")
@@ -57,15 +57,7 @@ def prepare_train(train_features,train_labels,validate_features,validate_labels,
     validate_set = torch.utils.data.TensorDataset(validate_features, validate_labels)
     validate_iter = torch.utils.data.DataLoader(validate_set, batch_size=batch_size,shuffle=False)
     train_iter = torch.utils.data.DataLoader(train_set, batch_size=batch_size,shuffle=True)
-    ## save the data
-    train_true_labels = []
-    train_pred_labels = []
 
-    validate_true_labels = []
-    validate_pred_labels = []
-
-    dict_train = {}
-    dict_validate = {}
     train_loss_list = []
     validate_loss_list = []
     for epoch in tqdm(range(num_epochs),"epoches: "):
@@ -87,13 +79,11 @@ def prepare_train(train_features,train_labels,validate_features,validate_labels,
 
             # append the data 
             t_score = train_scaler.inverse_transform(score.data.numpy())
-            train_pred_labels[(n-1)*batch_size:n*batch_size] = t_score.astype(int)
-            t_score = np.squeeze(np.ceil(t_score.reshape(1,-1))).astype(int)
+            t_score = np.squeeze(np.round(t_score.reshape(-1))).astype(int)
 
             # append the data 
             t_label = train_scaler.inverse_transform(label.data.numpy())
-            train_true_labels[(n-1)*batch_size:n*batch_size] = t_label.astype(int)
-            t_label = np.squeeze(t_label.reshape(1,-1)).astype(int)
+            t_label = np.squeeze(np.round(t_label.reshape(-1))).astype(int)
             
             train_acc += accuracy_score(t_label,t_score)
             train_loss += loss
@@ -108,23 +98,15 @@ def prepare_train(train_features,train_labels,validate_features,validate_labels,
 
                 # append the data
                 v_score = validate_scaler.inverse_transform(v_score)
-                validate_pred_labels[(m-1)*batch_size:m*batch_size] = v_score.astype(int)
-                v_score = np.squeeze(np.ceil(v_score.reshape(1,-1))).astype(int)
+                v_score = np.squeeze(np.round(v_score.reshape(1,-1))).astype(int)
 
                 # append the data
                 v_label = validate_scaler.inverse_transform(v_label)
-                validate_true_labels[(m-1)*batch_size:m*batch_size] = v_label.astype(int)
-                v_label = np.squeeze(v_label.reshape(1,-1)).astype(int)
+                v_label = np.squeeze(np.round(v_label.reshape(1,-1))).astype(int)
 
                 validate_acc += accuracy_score(v_label,v_score)
                 validate_losses += validate_loss
                 validate_loss_list.append(validate_loss.data.numpy().tolist())
-        dict_train['true'] = train_true_labels
-        dict_train['predict'] = train_pred_labels
-
-        dict_validate['true'] = validate_true_labels
-        dict_validate['predict'] = validate_pred_labels
-        
         end = time.time()
         runtime = end - start
         print('epoch: %d, train loss: %.4f, train acc: %.2f, validate loss: %.4f, validate acc: %.2f, time: %.2f' %
@@ -137,8 +119,6 @@ def prepare_train(train_features,train_labels,validate_features,validate_labels,
     model_save_file = os.path.join(save_file,modelname + ".pkl")
     net.to("cpu")
     torch.save(net,model_save_file)
-    # save the loss value
-    np.savez(os.path.join(save_file,"labels.npz"),train_data = dict_train,validate_data = dict_validate)
     return train_loss_list,validate_loss_list
 def load_datasets(filename):
     data = np.load(filename)
@@ -182,10 +162,8 @@ def train_entry(modelname):
         net = BiGRUNet(vocab_size, embed_size = config.word_dim,labels= config.labels,weight= weight,use_gpu = config.use_gpu)
     elif modelname == "MANNet":
         net = MANNet(vocab_size, embed_size = config.word_dim,encoder_size = 30,labels= config.labels,weight= weight,use_gpu = config.use_gpu)
-    elif modelname == "BiSRUNet":
-        net = BiSRU(vocab_size, embed_size = config.word_dim,encoder_size = 30,labels= config.labels,weight= weight,use_gpu = config.use_gpu)
     else:
-        raise Exception("unknown model")
+        raise Exception("unknown model:" + modelname)
     train_loss_list,validate_loss_list = prepare_train(train_features,train_labels,validate_features,validate_labels,train_scaler,validate_scaler,weight,net,
         vocab_size,modelname,config.num_epochs,config.batch_size,config.learning_rate)
     # draw pictures
@@ -203,73 +181,4 @@ def train_entry(modelname):
     plt.plot(x2,validate_loss_list)
     plt.title('validate loss',fontsize=12)
     plt.savefig(pic_validate)
-    plt.show()
-def test_entry(modelname):
-    # loading model
-    model_save_file = os.path.join(config.save_statics_file,modelname,modelname + ".pkl")
-    if os.path.exists(model_save_file):
-        net = torch.load(model_save_file)
-        net.to(config.device)
-    else:
-        print("There is no model in file: "+config.save_statics_file)
-        return 
-    
-    data = np.load(config.vocab_file)
-    word_to_idx = data['word_to_idx'][()]
-    # preparing test datasets
-    sentences_test,labels_test = load_datasets(config.test_npz)
-    test_features,test_labels,test_scaler = prepare_datasets(sentences_test,labels_test,word_to_idx,"test")
-
-    test_set = torch.utils.data.TensorDataset(test_features, test_labels)
-    test_iter = torch.utils.data.DataLoader(test_set, batch_size=config.batch_size,shuffle=False)
-
-    test_true_labels = test_labels.data.numpy()
-    test_length = len(test_labels)
-    test_pred_labels = np.zeros((test_length,config.labels))
-
-    start = time.time()
-    test_losses = 0
-    test_acc = 0
-    m = 0
-    test_loss_list = []
-    loss_function = nn.BCELoss()
-    dict_test = {}
-    with torch.no_grad():
-        for test_feature, test_label in tqdm(test_iter,"test: "):
-            m += 1
-            test_score = net(test_feature)
-            test_loss = loss_function(test_score, test_label)
-
-            te_label = test_label.data.numpy()
-            te_score = test_score.data.numpy()
-            # append the data
-            te_score = test_scaler.inverse_transform(te_score)
-            test_pred_labels[(m-1)*config.batch_size:m*config.batch_size] = te_score.astype(int)
-            te_score = np.squeeze(np.round(te_score.reshape(1,-1))).astype(int)
-
-            # append the data
-            te_label = test_scaler.inverse_transform(te_label)
-            test_true_labels[(m-1)*config.batch_size:m*config.batch_size] = te_label.astype(int)
-            te_label = np.squeeze(te_label.reshape(1,-1)).astype(int)
-
-            test_acc += accuracy_score(te_label,te_score)
-            test_losses += test_loss
-            test_loss_list.append(test_loss.data.numpy().tolist())
-    dict_test['true'] = test_true_labels
-    dict_test['predict'] = test_pred_labels
-    end = time.time()
-    runtime = end - start
-    print('test loss: %.4f, test acc: %.2f, time: %.2f' %(test_losses.data / m, test_acc / m, runtime))
-    # save the loss value
-    save_file = os.path.join(config.save_statics_file,modelname)
-    data = np.load(os.path.join(save_file,"labels.npz"))
-    list_train = data['train_data']
-    list_validate = data['validate_data']
-    np.savez(os.path.join(save_file,"labels.npz"),train_data = list_train,validate_data = list_validate,test_data = dict_test)
-    # draw pictures
-    pic_save_file = os.path.join(config.save_statics_file,modelname)
-    pic_test = os.path.join(pic_save_file,modelname+"_test"+".png")
-    x = np.linspace(0,len(test_loss_list)-1,len(test_loss_list))
-    plt.plot(x,test_loss_list,label = "train loss")
-    plt.savefig(pic_test)
     plt.show()
